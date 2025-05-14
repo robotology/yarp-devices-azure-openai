11c8dc2c0895f3853046456f493a0e0ac15d8f44
Ettore Landini <ettore.landini@iit.it>
2025-05-14 18:13:09 +0200

    TtsDevice paramsparser updated
    Compiles [Yes]; Works [No]

--- src/devices/ttsDevice/TtsDevice_ParamsParser.cpp
+++ src/devices/ttsDevice/TtsDevice_ParamsParser.cpp
@@ -8,7 +8,7 @@
 // This is an automatically generated file. Please do not edit it.
 // It will be re-generated if the cmake flag ALLOW_DEVICE_PARAM_PARSER_GERNERATION is ON.
 
-// Generated on: Wed May 14 11:47:39 2025
+// Generated on: Wed May 14 14:48:03 2025
 
 
 #include "TtsDevice_ParamsParser.h"
